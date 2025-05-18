59b12c5bb77c88b2c0d001d5c95637ee90d30c88
elruddy <lea.guzmicka@outlook.com>
2025-05-18 17:32:53 +0300

    added more functionality
    functions to reveal all cells per game requirements.
    added options for levels
    added option to mark on right click

--- js/utils.js
+++ js/utils.js
@@ -3,11 +3,23 @@ function renderBoard(mat, selector) {
   for (var i = 0; i < mat.length; i++) {
     strHTML += '<tr>';
     for (var j = 0; j < mat[0].length; j++) {
-      strHTML += `<td onclick="onCellClicked(this, ${i},  ${j})" id="cell-${i}-${j}" class="cell"></td>`;
+      strHTML += `<td oncontextmenu="onCellMarked(event, this, ${i}, ${j})" onclick="onCellClicked(this, ${i}, ${j})" id="cell-${i}-${j}" class="cell"></td>`;
     }
     strHTML += '</tr>';
   }
   strHTML += '</tbody></table>';
   const elContainer = document.querySelector(selector);
   elContainer.innerHTML = strHTML;
+}
+
+function revealNoneMineCell(elCell, i, j) {
+  gBoard[i][j].isRevealed = true;
+  elCell.classList.add(`around-${gBoard[i][j].minesAroundCount}`);
+  elCell.classList.add('revealed');
+  gGame.revealedCount++;
+  elCell.innerText = gBoard[i][j].minesAroundCount;
+
+  if (gBoard[i][j].minesAroundCount === 0) {
+    expandReveal(i, j);
+  }
 }
